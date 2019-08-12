1923f86f8e31d67a9aea6346fd3ee36321692167
Peerapong Pimup <draftutd@hotmail.com>
2019-08-13 00:18:52 +0700

    12/08/2562

--- basic/write_read_loop_with_config.c
+++ basic/write_read_loop_with_config.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <LabJackM.h>
 #include "../LJM_Utilities.h"
 int main()
-{
+{   
+	int hours, minutes, seconds, day, month, year;
 	int err;
 	int handle;
 	int i;
@@ -13,7 +16,7 @@ int main()
 	int fioState;
 	const int INTERVAL_HANDLE = 1;
 	int it = 0;
-	int msDelay = 1000;
+	int msDelay = 500;
 	enum { MAX_FRAMES = 2 };
 	int numFrames;
 	char * aNames[MAX_FRAMES];
@@ -22,48 +25,38 @@ int main()
 	ErrorCheck(err, "LJM_Open");
 	PrintDeviceInfoFromHandle(handle);
 	deviceType = GetDeviceType(handle);
-	if (deviceType == LJM_dtT4) {
-		WriteNameOrDie(handle, "DIO_INHIBIT", 0xF9F);
-		WriteNameOrDie(handle, "DIO_ANALOG_ENABLE", 0x000);
-		}
-	else {
-		WriteNameOrDie(handle, "AIN0_NEGATIVE_CH", 199);
-		WriteNameOrDie(handle, "AIN0_RANGE", 10);
-		}
+	WriteNameOrDie(handle, "DIO_INHIBIT", 0xF9F);
+	WriteNameOrDie(handle, "DIO_ANALOG_ENABLE", 0x000);
 	WriteNameOrDie(handle, "AIN0_RESOLUTION_INDEX", 0);
 	WriteNameOrDie(handle, "AIN0_SETTLING_US", 0);
 	err = LJM_StartInterval(INTERVAL_HANDLE, msDelay * 1000);
 	ErrorCheck(err, "LJM_StartInterval");
 	printf("\n");
 	while (1) {
-		numFrames = 2;
+		time_t now;
+		time(&now);
+		struct tm* local = localtime(&now);
+		hours = local->tm_hour;
+		minutes = local->tm_min;
+		seconds = local->tm_sec;
+		day = local->tm_mday;
+		month = local->tm_mon + 1;
+		year = local->tm_year + 1900;
+
+		numFrames = 3;
 		aNames[0] = "AIN0";
-		if (deviceType == LJM_dtT4) {
-			aNames[1] = "FIO6";
-		}
-		else {
-			aNames[1] = "FIO2";
-		}
-		dacVolt = it % 6; // 0-5
-		fioState = it % 2; // 0 or 1
-		aValues[0] = dacVolt;
-		aValues[1] = (double)fioState;
+		aNames[1] = "AIN1";
+		aNames[2] = "AIN2";
 		err = LJM_eReadNames(handle, numFrames, (const char **)aNames, aValues,
 			&errorAddress);
 		ErrorCheckWithAddress(err, errorAddress, "LJM_eReadNames");
-		printf("eReadNames  :");
-		for (i = 0; i < numFrames; i++) {
-			printf(" %s = %.4f  ", aNames[i], aValues[i]);
-		}
-		printf("\n");
-
-		++it;
-
+		printf("Date is : %02d/%02d/%d ", day, month, year);
+		printf("Time is : %02d:%02d:%02d am ", hours, minutes, seconds);
+		printf(" %s = %.4f  ", aNames[0], aValues[0]);
+		printf(" %s = %.4f  ", aNames[1], aValues[1]);
+		printf(" %s = %.4f  ", aNames[2], aValues[2]);
 		err = LJM_WaitForNextInterval(INTERVAL_HANDLE, &skippedIntervals);
 		ErrorCheck(err, "LJM_WaitForNextInterval");
-		if (skippedIntervals > 0) {
-			printf("SkippedIntervals: %d\n", skippedIntervals);
-		}
 		printf("\n");
 	}
 
